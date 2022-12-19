596be18f31ae44db34ae7a5e9290bb9ca803a961
MasAnton <antonprayogo002@gmail.com>
2022-12-20 02:23:42 +0700

    Menambahkan Toggle

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 saklar = (action, lamp) => {
   let toggle = document.getElementById("default-toggle");
+  let lampu1 = document.getElementById("lampu1");
+  let lampu2 = document.getElementById("lampu2");
+  let lampu3 = document.getElementById("lampu3");
 
   if (toggle.checked) {
     console.log("dinyaalakan");
@@ -7,32 +10,30 @@ saklar = (action, lamp) => {
     console.log("matikan");
   }
 
-  console.log("testing ngab", toggle.checked);
-  let lampu1 = document.getElementById("lampu1");
-  let lampu2 = document.getElementById("lampu2");
-  let lampu3 = document.getElementById("lampu3");
-
-  if (action == "on") {
-    if (lamp == 1) {
-      lampu1.src = "asset/img/on.gif";
-    }
-    if (lamp == 2) {
-      lampu2.src = "asset/img/on.gif";
-    }
-    if (lamp == 3) {
-      lampu3.src = "asset/img/on.gif";
-    }
+  if (toggle.checked) {
+    lampu1.src = "asset/img/on.gif";
   }
-
-  if (action == "off") {
-    if (lamp == 1) {
-      lampu1.src = "asset/img/off.gif";
-    }
-    if (lamp == 2) {
-      lampu2.src = "asset/img/off.gif";
-    }
-    if (lamp == 3) {
-      lampu3.src = "asset/img/off.gif";
-    }
+  //     if (lamp == 1) {
+  //       lampu1.src = "asset/img/on.gif";
+  //     }
+  //     if (lamp == 2) {
+  //       lampu2.src = "asset/img/on.gif";
+  //     }
+  //     if (lamp == 3) {
+  //       lampu3.src = "asset/img/on.gif";
+  //     }
+  //   }
+  else {
+    lampu1.src = "asset/img/off.gif";
+    //   if (lamp == 1) {
+    //     lampu1.src = "asset/img/off.gif";
+    //   }
+    //   if (lamp == 2) {
+    //     lampu2.src = "asset/img/off.gif";
+    //   }
+    //   if (lamp == 3) {
+    //     lampu3.src = "asset/img/off.gif";
+    //   }
+    // };
   }
 };
